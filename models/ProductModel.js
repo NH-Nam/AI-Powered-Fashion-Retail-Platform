4e08bd13a6cdbda948fe8a4419d81c882daee1e4
LeeDucAnh <ducanh1754@gmail.com>
2025-08-18 18:40:03 +0700

    Adding new model files: InventoryModel, WarehouseModel for warehouse management. Also add more fields to CartModel, OrderModel and ProductModel. Also add a PurchaseLog model

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -19,13 +19,73 @@ var ProductSchema = mongoose.Schema({
         type: Number,
         required: true
     },
+    buy_price: {
+        type: Number,
+        default: 0
+    },
     discount_price: {
         type: Number,
         default: 0
     },
+    // Fashion-specific attributes for AI recognition
+    size: {
+        type: String,
+        default: '',
+        enum: ['', 'XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', 'One Size', 'Custom']
+    },
     color: {
         type: String,
+        default: '',
+        enum: ['', 'Black', 'White', 'Red', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple', 'Orange', 'Brown', 'Gray', 'Navy', 'Beige', 'Cream', 'Multi-color', 'Other']
+    },
+    // Variant-level inventory like Shopee (multiple size-color-quantity combos)
+    variants: [{
+        size: {
+            type: String,
+            enum: ['', 'XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', 'One Size', 'Custom']
+        },
+        color: {
+            type: String,
+            enum: ['', 'Black', 'White', 'Red', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple', 'Orange', 'Brown', 'Gray', 'Navy', 'Beige', 'Cream', 'Multi-color', 'Other']
+        },
+        quantity: {
+            type: Number,
+            min: 0,
+            default: 0
+        }
+    }],
+    // Multiple materials with percentages
+    materials: [{
+        material: {
+            type: String,
+            enum: ['Cotton', 'Polyester', 'Wool', 'Silk', 'Linen', 'Denim', 'Leather', 'Suede', 'Synthetic', 'Blend', 'Other']
+        },
+        percentage: {
+            type: Number,
+            min: 0,
+            max: 100,
+            default: 100
+        }
+    }],
+    // Additional fashion attributes
+    brand: {
+        type: String,
         default: ''
+    },
+    style: {
+        type: String,
+        default: '',
+        enum: ['Casual', 'Formal', 'Sport', 'Vintage', 'Modern', 'Classic', 'Trendy', 'Bohemian', 'Minimalist', 'Other']
+    },
+    // Multiple seasons
+    seasons: [{
+        type: String,
+        enum: ['Spring', 'Summer', 'Fall', 'Winter', 'All Season']
+    }],
+    gender: {
+        type: String,
+        default: 'Unisex',
+        enum: ['Men', 'Women', 'Unisex', 'Kids']
     },
     deleted:{
         type: Number,
